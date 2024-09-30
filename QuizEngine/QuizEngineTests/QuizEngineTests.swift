7c7a833423d136fee98f09c28d8afc9167e82864
gtsofa <jlsnyule@yahoo.com>
2024-09-30 21:33:48 +0300

    add flow chart diagram

---
+++ QuizEngine/QuizEngineTests/QuizEngineTests.swift
@@ -0,0 +1,15 @@
+//
+//  QuizEngineTests.swift
+//  QuizEngineTests
+//
+//  Created by Julius on 30/09/2024.
+//
+
+import XCTest
+
+final class QuizEngineTests: XCTestCase {
+    func test() {
+        XCTFail("failing tests")
+    }
+
+}
