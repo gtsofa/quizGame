3c33c42ab854a200137ee95b4088537300445383
gtsofa <jlsnyule@yahoo.com>
2024-09-30 22:38:36 +0300

    Remove the `!` by unwrapping the first question

--- QuizEngine/QuizEngineTests/QuizEngineTests.swift
+++ QuizEngine/QuizEngineTests/QuizEngineTests.swift
@@ -21,8 +21,8 @@ class Flow {
     }
     
     func start() {
-        if !questions.isEmpty {
-            router.routeTo(question: "Q1")
+        if let firstQuestion = questions.first {
+            router.routeTo(question: firstQuestion)
         }
     }
 }
