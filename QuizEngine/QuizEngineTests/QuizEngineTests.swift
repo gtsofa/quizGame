9e62e3d56ef595f8a929e74100a9c0387254a520
gtsofa <jlsnyule@yahoo.com>
2024-09-30 21:55:02 +0300

    Routed questions is nil on inialization

--- QuizEngine/QuizEngineTests/QuizEngineTests.swift
+++ QuizEngine/QuizEngineTests/QuizEngineTests.swift
@@ -7,9 +7,26 @@
 
 import XCTest
 
+protocol Router {}
+
+class Flow {
+    private let router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+}
+
 final class QuizEngineTests: XCTestCase {
-    func test() {
-        XCTFail("failing tests")
+    func test_init_doesNotRouteToQuestion() {
+        let router = RouterSpy()
+        _ = Flow(router: router)
+        
+        XCTAssertEqual(router.routedQuestions, 0)
+    }
+    
+    class RouterSpy: Router {
+        var routedQuestions = 0
     }
 
 }
