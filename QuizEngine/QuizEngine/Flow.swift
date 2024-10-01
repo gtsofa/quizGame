c4e80446f6e4794ad2cd4b2927d0079cf8ea7f3a
gtsofa <jlsnyule@yahoo.com>
2024-10-01 11:07:18 +0300

    Move `Flow` to its own file in production

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol Router {
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+}
+
+class Flow {
+    private let questions: [String]
+    private let router: Router
+    
+    init(questions: [String], router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start() {
+        if let firstQuestion = questions.first {
+            router.routeTo(question: firstQuestion) { [weak self] _ in
+                guard let strongSelf = self else { return }
+                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
+                let nextQuestion = strongSelf.questions[firstQuestionIndex+1]
+                strongSelf.router.routeTo(question: nextQuestion) { _ in }
+            }
+        }
+    }
+}
--- QuizEngine/QuizEngineTests/QuizEngineTests.swift
+++ QuizEngine/QuizEngineTests/QuizEngineTests.swift
@@ -6,31 +6,7 @@
 //
 
 import XCTest
-
-protocol Router {
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
-}
-
-class Flow {
-    private let questions: [String]
-    private let router: Router
-    
-    init(questions: [String], router: Router) {
-        self.questions = questions
-        self.router = router
-    }
-    
-    func start() {
-        if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion) { [weak self] _ in
-                guard let strongSelf = self else { return }
-                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
-                let nextQuestion = strongSelf.questions[firstQuestionIndex+1]
-                strongSelf.router.routeTo(question: nextQuestion) { _ in }
-            }
-        }
-    }
-}
+@testable import QuizEngine
 
 final class QuizEngineTests: XCTestCase {
     func test_start_withNoQuestionDoesNotRouteToQuestion() {
